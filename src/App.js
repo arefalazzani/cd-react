4d58d8f7dca8a8270058772e7e7a9299b28586b8
Aref Alazzani <aref.alazzani@gmail.com>
2026-09-04 13:08:35 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          //href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXe-jnO3HorOsHCZc6cMWYVrvz7pucVfJvSGZbq6Q4Dg&s"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
